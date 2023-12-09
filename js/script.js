a4619d65ec56a51aa0f1bb5432c8ffbc014ea125
Xiaofeng Zhao <xiaofeng.zhao@medtronic.com>
2023-12-09 14:25:50 -0700

    remove log in handleMove

--- js/script.js
+++ js/script.js
@@ -41,13 +41,8 @@ function handleMove(evt) {
     const idx = ongoingTouchIndexById(touches[i].identifier);
 
     if (idx >= 0) {
-      log(`continuing touch ${idx}`);
       ctx.beginPath();
-      log(
-        `ctx.moveTo( ${ongoingTouches[idx].pageX}, ${ongoingTouches[idx].pageY} );`,
-      );
       ctx.moveTo(ongoingTouches[idx].pageX, ongoingTouches[idx].pageY);
-      log(`ctx.lineTo( ${touches[i].pageX}, ${touches[i].pageY} );`);
       ctx.lineTo(touches[i].pageX, touches[i].pageY);
       ctx.lineWidth = 4;
       ctx.strokeStyle = color;
